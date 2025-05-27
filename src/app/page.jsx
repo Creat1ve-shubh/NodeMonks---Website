4851674d8fb6c2c3d0ead93b71cbb904cbdb72a8
Creat1ve <shubhdayal.shrivastava@gmail.com>
2025-05-28 01:43:03 +0530

    adding lamp - homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,13 +3,13 @@ import ComingSoon from "@/components/Home/ComingSoon";
 import ContactUs from "@/components/Home/ContactUs";
 import HeroSection from "@/components/Home/HeroSection";
 import Works from "@/components/Home/WorksGrid";
+import { LampDemo } from "@/components/ui/lamp";
 
 export default function Home() {
   return (
     <div className="overflow-x-hidden scroll-smooth">
       <HeroSection />
-      <ComingSoon />
-
+      <LampDemo />
       <AboutUs />
       <ContactUs />
     </div>
